e735eb9856765a84b1e70bd9a3bf092909dc45b8
DS <dsrathorebsw1234@gmail.com>
2024-01-18 18:01:15 +0530

    Hands on service object

--- App/index.js
+++ App/index.js
@@ -1,9 +1,6 @@
 import express from "express";
-import dotenv from 'dotenv';
 
-dotenv.config({ path: "../.env" });
-
-const PORT = process.env.PORT || 5000;
+const PORT = 4000;
 
 const app = express();
 
