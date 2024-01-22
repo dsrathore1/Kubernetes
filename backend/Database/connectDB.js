4a1f313400a12fce64598305e73e2418b7d561da
DS <dsrathorebsw1234@gmail.com>
2024-01-22 13:07:20 +0530

    Configured Backend

--- backend/Database/connectDB.js
+++ backend/Database/connectDB.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017' || process.env.MONGODB_URI_K8S
 
 const connectDB = async () => {
     await mongoose.connect(MONGODB_URI, {
